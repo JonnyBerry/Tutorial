12a6056f1c24013848cd00a9ab0f3871c249e58d
jonathanberry <jonathanberry45@gmail.com>
2022-02-06 03:08:15 +0000

    booklist added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
+//css
 
-function Greeting(){
-  return <h4>this is jonathan and this is my first component</h4>;
+import './index.css';
+//function Greeting(){
+//  return <h4>this is jonathan and this is my first component</h4>;
+//}
+
+function Booklist() {
+  return (
+  <section className='booklist'>
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  </section>
+  );
 }
 
+const Book = () =>{
+  return (
+  <article>
+    <Image></Image>
+    <Title/>
+    <Author/>
+  </article>
+  );
+};
 
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+
+const Title = () => <h1>Manifest</h1>
+const Author = () => <h4>Roxie Nafousi </h4>
+
+const Image = () => <img src='https://m.media-amazon.com
+/images/I/715u7p+38+L._AC_UY436_QL65_.jpg'alt=''/>
+
+ReactDom.render(<Booklist />, document.getElementById('root'));
