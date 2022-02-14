e8a879f4dad343709445f973abb8dae4f42fafaa
jonathanberry <jonathanberry45@gmail.com>
2022-02-14 21:36:40 +0000

    added second book to list

--- src/index.js
+++ src/index.js
@@ -7,34 +7,52 @@ import './index.css';
 //function Greeting(){
 //  return <h4>this is jonathan and this is my first component</h4>;
 //}
+const firstBook = {
+  img:'https://m.media-amazon.com/images/I/715u7p+38+L._AC_UY436_QL65_.jpg',
+  title:'Manifest',
+  author:'Roxie Nafousi',
+}
+const secondBook = {
+  img:'https://m.media-amazon.com/images/I/81huXG+mTmL._AC_UY436_QL65_.jpg',
+  title:'The Mental Toughness Handbook',
+  author:'Damon Zahariades',
+}
+
 
 function Booklist() {
   return (
   <section className='booklist'>
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
+  <Book
+    img={firstBook.img}
+    title={firstBook.title}
+    author= {firstBook.author}
+    >
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Sit in magni ad molestias minima fugit voluptates a,
+        ipsam rem eaque.</p>
+  </Book>
+  <Book
+    img={secondBook.img}
+    title={secondBook.title}
+    author={secondBook.author}
+    />
   </section>
   );
 }
 
-const Book = () =>{
+const Book = (props) => {
+  const {img, title, author} = props;
+  console.log(props);
   return (
-  <article>
-    <Image></Image>
-    <Title/>
-    <Author/>
-  </article>
+  <article className="book">
+  <img src={img} alt=''/>
+  <h1>{title}</h1>
+  <h4>{author}</h4>
+  {props.children}
+</article>
   );
 };
 
 
-const Title = () => <h1>Manifest</h1>
-const Author = () => <h4>Roxie Nafousi </h4>
-
-const Image = () => <img src='https://m.media-amazon.com
-/images/I/715u7p+38+L._AC_UY436_QL65_.jpg'alt=''/>
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
