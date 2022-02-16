6b0b4d9c1618920f485d1790480345a9465a3be1
jonathanberry <jonathanberry45@gmail.com>
2022-02-16 00:54:05 +0000

    refactoring booklist

--- src/index.js
+++ src/index.js
@@ -7,48 +7,46 @@ import './index.css';
 //function Greeting(){
 //  return <h4>this is jonathan and this is my first component</h4>;
 //}
-const firstBook = {
-  img:'https://m.media-amazon.com/images/I/715u7p+38+L._AC_UY436_QL65_.jpg',
+const book= [
+ {
+  img:
+  'https://m.media-amazon.com/images/I/715u7p+38+L._AC_UY436_QL65_.jpg',
   title:'Manifest',
   author:'Roxie Nafousi',
-}
-const secondBook = {
-  img:'https://m.media-amazon.com/images/I/81huXG+mTmL._AC_UY436_QL65_.jpg',
-  title:'The Mental Toughness Handbook',
-  author:'Damon Zahariades',
-}
+},
 
+  {
+    img:
+    'https://m.media-amazon.com/images/I/81huXG+mTmL._AC_UY436_QL65_.jpg',
+    title:'The Mental Toughness Handbook',
+    author:'Damon Zahariades',
+  },
+];
 
 function Booklist() {
-  return (
-  <section className='booklist'>
-  <Book
-    img={firstBook.img}
-    title={firstBook.title}
-    author= {firstBook.author}
-    >
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Sit in magni ad molestias minima fugit voluptates a,
-        ipsam rem eaque.</p>
-  </Book>
-  <Book
-    img={secondBook.img}
-    title={secondBook.title}
-    author={secondBook.author}
-    />
+  return(
+     <section className='booklist'>
+    {book.map((book) => {
+   const {img, title, author} = book;
+   return (
+     <div>
+       <h3>{title}</h3>
+       <h6>{author}</h6>
+     </div>
+   );
+  })}
   </section>
   );
 }
 
 const Book = (props) => {
-  const {img, title, author} = props;
+  const {img, title, author,} = props;
   console.log(props);
   return (
   <article className="book">
   <img src={img} alt=''/>
   <h1>{title}</h1>
   <h4>{author}</h4>
-  {props.children}
 </article>
   );
 };
