e9f5aa8a0d334c40c10c1d46a95f2a708dcaea68
jonathanberry <jonathanberry45@gmail.com>
2022-02-20 17:21:38 +0000

    books component added in seperate file

--- src/index.js
+++ src/index.js
@@ -45,16 +45,27 @@ const Book = ({img, title,author}) => {
   //console.log(props);
   //need to set up an event in react- attribute and eventHandler
   //onClick, onMouseOver
-  const clickHandler = () =>{
-    alert('hello world')
+  const clickHandler = (e) =>{
+    console.log(e);
+    console.log(e.target);
+    alert('hello world');
+  };
+  const complexExample = (author) => {
+    console.log (author);
   }
   return (
-  <article className="book">
+  <article className="book"
+   onMouseOver={()=> {
+    console.log(title);
+  }}
+  >
   <img src={img} alt=''/>
-  <h1>{title}</h1>
+  <h1 onClick={()=>console.log(title)}>{title}</h1>
   <h4>{author}</h4>
   <button type="button" onClick={clickHandler}>
     reference example
+  </button>
+  <button type="button" onClick={() =>complexExample(author)}>more complex example
   </button>
 </article>
   );
