405bf2e942e88a1cc5d3af00bcb68914aaf0fd7d
jonathanberry <jonathanberry45@gmail.com>
2022-02-21 15:14:29 +0000

    added seperate book and books files in scr

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+ {id:1,
+  img:
+  'https://m.media-amazon.com/images/I/715u7p+38+L._AC_UY436_QL65_.jpg',
+  title:'Manifest',
+  author:'Roxie Nafousi',
+},
+
+  {id:2,
+    img:
+    'https://m.media-amazon.com/images/I/81huXG+mTmL._AC_UY436_QL65_.jpg',
+    title:'The Mental Toughness Handbook',
+    author:'Damon Zahariades',
+  },
+  {id:3,
+    img:
+    'https://m.media-amazon.com/images/I/81DPTref3fL._AC_UY436_QL65_.jpg',
+    title:'Harry Potter and the Philosophers Stone',
+    author:'J.K. Rowling ',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -4,35 +4,16 @@ import ReactDom from 'react-dom'
 //css
 
 import './index.css';
+
+import {books} from './books'
+import Book from './Book'
 //function Greeting(){
 //  return <h4>this is jonathan and this is my first component</h4>;
 //}
-const book= [
- {id:1,
-  img:
-  'https://m.media-amazon.com/images/I/715u7p+38+L._AC_UY436_QL65_.jpg',
-  title:'Manifest',
-  author:'Roxie Nafousi',
-},
-
-  {id:2,
-    img:
-    'https://m.media-amazon.com/images/I/81huXG+mTmL._AC_UY436_QL65_.jpg',
-    title:'The Mental Toughness Handbook',
-    author:'Damon Zahariades',
-  },
-  {id:3,
-    img:
-    'https://m.media-amazon.com/images/I/81DPTref3fL._AC_UY436_QL65_.jpg',
-    title:'Harry Potter and the Philosophers Stone',
-    author:'J.K. Rowling ',
-  },
-];
-
 function Booklist() {
   return(
      <section className='booklist'>
-    {book.map((book, index) => {
+    {books.map((book, index) => {
    return <Book key={book.id} {...book}></Book>;
   })}
   </section>
@@ -40,36 +21,6 @@ function Booklist() {
 }
 
 //book component
-const Book = ({img, title,author}) => {
-  //const {img, title, author} = props;
-  //console.log(props);
-  //need to set up an event in react- attribute and eventHandler
-  //onClick, onMouseOver
-  const clickHandler = (e) =>{
-    console.log(e);
-    console.log(e.target);
-    alert('hello world');
-  };
-  const complexExample = (author) => {
-    console.log (author);
-  }
-  return (
-  <article className="book"
-   onMouseOver={()=> {
-    console.log(title);
-  }}
-  >
-  <img src={img} alt=''/>
-  <h1 onClick={()=>console.log(title)}>{title}</h1>
-  <h4>{author}</h4>
-  <button type="button" onClick={clickHandler}>
-    reference example
-  </button>
-  <button type="button" onClick={() =>complexExample(author)}>more complex example
-  </button>
-</article>
-  );
-};
 
 
 
